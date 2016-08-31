ac8d38993b8f462e91c050a903d2296722347443
DESKTOP-PLPG04M\josh mobile <joshua.wetzel-2@selu.edu>
2016-08-31 12:22:57 -0500

    Created Template asp.net web application

---
+++ cmps285HotelProject/cmps285HotelProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cmps285HotelProject.Startup))]
+namespace cmps285HotelProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
