4218576c80e9a2143c72c6bb72007ab2c2462aa3
Joshua Wetzel <joshua.wetzel-2@selu.edu>
2016-10-05 03:02:54 -0500

    HouseKeeping page progres, linq statement works

---
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Models/HouseKeepingModels/HouseKeepingFloorsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HotelIntegratedComputerSystems.Models.HouseKeepingModels
+{
+    public class HouseKeepingFloorsViewModel
+    {
+        public List<RoomsViewModel> RoomsOnFloor { get; set; }
+        public int RoomId { get; set; }
+    }
+}
