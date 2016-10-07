e91006a3926d2d48dfcebd295d86bc2429935d1f
Joshua Wetzel <joshua.wetzel-2@selu.edu>
2016-10-07 13:30:51 -0500

    Repaired Rooms controller created view model for HouseKeeping created Controller for HouseKeeping creaed views index enabled change housekeeping status to clean

---
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Controllers/HouseKeepingViewModelsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HotelIntegratedComputerSystems.Models;
+using System.Web.UI;
+
+namespace HotelIntegratedComputerSystems.Controllers
+{
+    public class HouseKeepingViewModelsController : Controller
+    {
+        private HicsTestDbEntities1 db = new HicsTestDbEntities1();
+
+        // GET: HouseKeepingViewModels
+        public ActionResult Index()
+        {
+            return View(db.GetRoomsForHouseKeeping());
+        }
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room ChangeRoom = db.Rooms.Where(s => s.Id == id).FirstOrDefault<Room>();
+            ChangeRoom.HousekeepingStatusId = 1;
+
+            db.Entry(ChangeRoom).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: HouseKeepingViewModels/Delete/5
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Models/HouseKeepingViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace HotelIntegratedComputerSystems.Models
+{
+    public class HouseKeepingViewModel
+    {
+        [System.Web.Mvc.HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [System.Web.Mvc.HiddenInput(DisplayValue = false)]
+        public int BuildingId { get; set; }
+
+        [DisplayName("Building")]
+        public string BuildingName { get; set; } //building table
+
+        [System.Web.Mvc.HiddenInput(DisplayValue = false)]
+        public int HousekeepingStatusId { get; set; }
+
+        [DisplayName("Clean Status")]
+        public string HousekeepingCleanStatus { get; set; } //housekeeping table
+
+        [DisplayName("Floor")]
+        public int FloorNumber { get; set; }
+
+        [DisplayName("Room")]
+        public int RoomNumber { get; set; }
+
+        [System.Web.Mvc.HiddenInput(DisplayValue = false)]
+        public int RoomStatusId { get; set; }
+
+        [DisplayName("Room Status")]
+        public string RoomStatusDescription { get; set; } //RoomStatus table
+    }
+}
