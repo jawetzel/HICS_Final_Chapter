2284997e11b87396f4fae1917c89f44ebc905dc1
Joshua Wetzel <joshua.wetzel-2@selu.edu>
2016-10-14 18:28:03 -0500

    Recreated RoomType page with services and viewmodel

--- cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Controllers/Admin/RoomTypesController.cs
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Controllers/Admin/RoomTypesController.cs
@@ -3,119 +3,81 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using HotelIntegratedComputerSystems.Models;
+using HotelIntegratedComputerSystems.Models.Admin;
+using HotelIntegratedComputerSystems.Services.Admin;
 
 namespace HotelIntegratedComputerSystems.Controllers.Admin
 {
     public class RoomTypesController : Controller
     {
-        private readonly HicsTestDbEntities1 _db = new HicsTestDbEntities1();
+        private readonly RoomTypeServices _services = new RoomTypeServices();
 
-        // GET: RoomTypes
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
-            return View(await _db.RoomTypes.ToListAsync());
+            return View(_services.GetRoomTypesList());
         }
 
-        // GET: RoomTypes/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var roomType = await _db.RoomTypes.FindAsync(id);
-            if (roomType == null)
-            {
-                return HttpNotFound();
-            }
-            return View(roomType);
-        }
-
-        // GET: RoomTypes/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: RoomTypes/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "Id,Bedding,Kitchen,Rooms,Bathrooms,SleepsVolume,NightlyRate")] RoomType roomType)
+        public ActionResult Create([Bind(Include = "Id,Bedding,Kitchen,Rooms,BathRooms,SleepsVolume,NightlyRate")] RoomTypeViewModel roomTypeViewModel)
         {
-            if (!ModelState.IsValid) return View(roomType);
-            _db.RoomTypes.Add(roomType);
-            await _db.SaveChangesAsync();
+            if (!ModelState.IsValid) return View(roomTypeViewModel);
+            _services.CreateNewRoomType(roomTypeViewModel);
             return RedirectToAction("Index");
         }
 
-        // GET: RoomTypes/Edit/5
-        public async Task<ActionResult> Edit(int? id)
+        public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var roomType = await _db.RoomTypes.FindAsync(id);
-            if (roomType == null)
+            var roomTypeViewModel = _services.FindEntryById(id.Value);
+            if (roomTypeViewModel == null)
             {
                 return HttpNotFound();
             }
-            return View(roomType);
+            return View(roomTypeViewModel);
         }
 
-        // POST: RoomTypes/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,Bedding,Kitchen,Rooms,Bathrooms,SleepsVolume,NightlyRate")] RoomType roomType)
+        public ActionResult Edit([Bind(Include = "Id,Bedding,Kitchen,Rooms,BathRooms,SleepsVolume,NightlyRate")] RoomTypeViewModel roomTypeViewModel)
         {
-            if (!ModelState.IsValid) return View(roomType);
-            _db.Entry(roomType).State = EntityState.Modified;
-            await _db.SaveChangesAsync();
+            if (!ModelState.IsValid) return View(roomTypeViewModel);
+            _services.PostChangesForEdit(roomTypeViewModel);
             return RedirectToAction("Index");
         }
 
-        // GET: RoomTypes/Delete/5
-        public async Task<ActionResult> Delete(int? id)
+        public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var roomType = await _db.RoomTypes.FindAsync(id);
-            if (roomType == null)
+            var roomTypeViewModel = _services.FindEntryById(id.Value);
+            if (roomTypeViewModel == null)
             {
                 return HttpNotFound();
             }
-            return View(roomType);
+            return View(roomTypeViewModel);
         }
 
-        // POST: RoomTypes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
+        public ActionResult DeleteConfirmed(int id)
         {
-            var roomType = await _db.RoomTypes.FindAsync(id);
-            _db.RoomTypes.Remove(roomType);
-            await _db.SaveChangesAsync();
+            _services.DeleteEntry(id);
             return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
