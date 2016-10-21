f8eab82914549f377507b532cee1761ae38d85b6
Joshua Wetzel <joshua.wetzel-2@selu.edu>
2016-10-20 23:08:21 -0500

    resolved renaming errors encured when swaping a view from service folder and a service from view folder

--- cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Services/MaidService/MaidServiceServices.cs
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Services/MaidService/MaidServiceServices.cs
@@ -2,6 +2,7 @@
 using System.Data.Entity;
 using System.Linq;
 using HotelIntegratedComputerSystems.Models;
+using HotelIntegratedComputerSystems.Models.MaidService;
 
 namespace HotelIntegratedComputerSystems.Services.MaidService
 {
