3c2ae2d6f24a3897d16f8450247c7189734860c2
Joshua Wetzel <joshua.wetzel-2@selu.edu>
2016-10-24 10:09:52 -0500

    abstracted filter function of 1 search box per column and applied it to rooms page

---
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Scripts/OneFilterPerColumn.js
@@ -0,0 +1,230 @@
+﻿function filterFunctionColumn1() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn2() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn3() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+    var filter3 = document.getElementById("myInput3").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+        checkForfilter(2, filter3, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn4() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+    var filter3 = document.getElementById("myInput3").value.toUpperCase();
+    var filter4 = document.getElementById("myInput4").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+        checkForfilter(2, filter3, tableRow[i]);
+        checkForfilter(3, filter4, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn5() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+    var filter3 = document.getElementById("myInput3").value.toUpperCase();
+    var filter4 = document.getElementById("myInput4").value.toUpperCase();
+    var filter5 = document.getElementById("myInput5").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+        checkForfilter(2, filter3, tableRow[i]);
+        checkForfilter(3, filter4, tableRow[i]);
+        checkForfilter(4, filter5, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn6() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+    var filter3 = document.getElementById("myInput3").value.toUpperCase();
+    var filter4 = document.getElementById("myInput4").value.toUpperCase();
+    var filter5 = document.getElementById("myInput5").value.toUpperCase();
+    var filter6 = document.getElementById("myInput6").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+        checkForfilter(2, filter3, tableRow[i]);
+        checkForfilter(3, filter4, tableRow[i]);
+        checkForfilter(4, filter5, tableRow[i]);
+        checkForfilter(5, filter6, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn7() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+    var filter3 = document.getElementById("myInput3").value.toUpperCase();
+    var filter4 = document.getElementById("myInput4").value.toUpperCase();
+    var filter5 = document.getElementById("myInput5").value.toUpperCase();
+    var filter6 = document.getElementById("myInput6").value.toUpperCase();
+    var filter7 = document.getElementById("myInput7").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+        checkForfilter(2, filter3, tableRow[i]);
+        checkForfilter(3, filter4, tableRow[i]);
+        checkForfilter(4, filter5, tableRow[i]);
+        checkForfilter(5, filter6, tableRow[i]);
+        checkForfilter(6, filter7, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn8() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+    var filter3 = document.getElementById("myInput3").value.toUpperCase();
+    var filter4 = document.getElementById("myInput4").value.toUpperCase();
+    var filter5 = document.getElementById("myInput5").value.toUpperCase();
+    var filter6 = document.getElementById("myInput6").value.toUpperCase();
+    var filter7 = document.getElementById("myInput7").value.toUpperCase();
+    var filter8 = document.getElementById("myInput8").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+        checkForfilter(2, filter3, tableRow[i]);
+        checkForfilter(3, filter4, tableRow[i]);
+        checkForfilter(4, filter5, tableRow[i]);
+        checkForfilter(5, filter6, tableRow[i]);
+        checkForfilter(6, filter7, tableRow[i]);
+        checkForfilter(7, filter8, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn9() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+    var filter3 = document.getElementById("myInput3").value.toUpperCase();
+    var filter4 = document.getElementById("myInput4").value.toUpperCase();
+    var filter5 = document.getElementById("myInput5").value.toUpperCase();
+    var filter6 = document.getElementById("myInput6").value.toUpperCase();
+    var filter7 = document.getElementById("myInput7").value.toUpperCase();
+    var filter8 = document.getElementById("myInput8").value.toUpperCase();
+    var filter9 = document.getElementById("myInput9").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+        checkForfilter(2, filter3, tableRow[i]);
+        checkForfilter(3, filter4, tableRow[i]);
+        checkForfilter(4, filter5, tableRow[i]);
+        checkForfilter(5, filter6, tableRow[i]);
+        checkForfilter(6, filter7, tableRow[i]);
+        checkForfilter(7, filter8, tableRow[i]);
+        checkForfilter(8, filter9, tableRow[i]);
+    }
+}
+
+function filterFunctionColumn10() {
+    var filter1 = document.getElementById("myInput1").value.toUpperCase();
+    var filter2 = document.getElementById("myInput2").value.toUpperCase();
+    var filter3 = document.getElementById("myInput3").value.toUpperCase();
+    var filter4 = document.getElementById("myInput4").value.toUpperCase();
+    var filter5 = document.getElementById("myInput5").value.toUpperCase();
+    var filter6 = document.getElementById("myInput6").value.toUpperCase();
+    var filter7 = document.getElementById("myInput7").value.toUpperCase();
+    var filter8 = document.getElementById("myInput8").value.toUpperCase();
+    var filter9 = document.getElementById("myInput9").value.toUpperCase();
+    var filter10 = document.getElementById("myInput10").value.toUpperCase();
+
+    var tableRow = document.getElementById("dev-table").getElementsByTagName("tr");
+
+    for (var i = 0; i < tableRow.length; i++) {
+        tableRow[i].style.display = "";
+
+        checkForfilter(0, filter1, tableRow[i]);
+        checkForfilter(1, filter2, tableRow[i]);
+        checkForfilter(2, filter3, tableRow[i]);
+        checkForfilter(3, filter4, tableRow[i]);
+        checkForfilter(4, filter5, tableRow[i]);
+        checkForfilter(5, filter6, tableRow[i]);
+        checkForfilter(6, filter7, tableRow[i]);
+        checkForfilter(7, filter8, tableRow[i]);
+        checkForfilter(8, filter9, tableRow[i]);
+        checkForfilter(9, filter10, tableRow[i]);
+    }
+}
+
+function checkForfilter(column, filter, tableRow) {
+    if (filter != "") {
+        FilterTheColumn(column, filter, tableRow);
+    }
+}
+
+function FilterTheColumn(columnIndex, filter, tableRow) {
+    var column = tableRow.getElementsByTagName("td")[columnIndex];
+
+    if (column) {
+        if ((column.innerHTML.toUpperCase().indexOf(filter) > -1) && (tableRow.style.display == "")) {
+            tableRow.style.display = "";
+        }
+        else if ((column.innerHTML.toUpperCase().indexOf(filter) > -1) && (tableRow.style.display == "none")) {
+        }
+        else {
+            tableRow.style.display = "none";
+        }
+    }
+}
