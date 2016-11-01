5f53c7a827c76dada0d0b53d252747dfaea7efe2
Joshua Wetzel <joshua.wetzel-2@selu.edu>
2016-11-01 08:48:33 -0500

    maintenanelog servies done, working on maintenanelog controler

---
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Controllers/Admin/MaintenanceLogViewModelsController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HotelIntegratedComputerSystems.Models;
+using HotelIntegratedComputerSystems.Models.Admin.MaintenanceLog;
+using HotelIntegratedComputerSystems.Services.Admin;
+
+namespace HotelIntegratedComputerSystems.Controllers.Admin
+{
+    public class MaintenanceLogViewModelsController : BaseController
+    {
+        private MaintenanceLogServices _services = new MaintenanceLogServices();
+
+        public ActionResult Index()
+        {
+            return View(_services.GetMaintenanceLogList());
+        }
+
+        public ActionResult Create()
+        {
+            return View(_services.RoomsForMaintenaneLogCreateEdit());
+        }
+
+        // POST: MaintenanceLogViewModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,RoomId,BuildingId,BuildingName,Floor,RoomNumber,Description,Date,MaintenanceTypeId,MaintenanceType")] MaintenanceLogViewModel maintenanceLogViewModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.MaintenanceLogViewModels.Add(maintenanceLogViewModel);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(maintenanceLogViewModel);
+        //}
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var maintLog = _services.FindEntryById(id.Value);
+            var roomsLog = _services.RoomsForMaintenaneLogCreateEdit();
+            maintLog.RoomsList = roomsLog.RoomsList;
+            if (maintLog.MaintenanceLog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maintLog);
+        }
+
+        // POST: MaintenanceLogViewModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,RoomId,BuildingId,BuildingName,Floor,RoomNumber,Description,Date,MaintenanceTypeId,MaintenanceType")] MaintenanceLogViewModel maintenanceLogViewModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(maintenanceLogViewModel).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(maintenanceLogViewModel);
+        //}
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var maintenanceLogViewModel = _services.FindEntryById(id.Value);
+            if (maintenanceLogViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maintenanceLogViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _services.DeleteEntry(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Services/Admin/MaintenanceLogServices.cs
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Services/Admin/MaintenanceLogServices.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using HotelIntegratedComputerSystems.Models;
+using HotelIntegratedComputerSystems.Models.Admin;
 using HotelIntegratedComputerSystems.Models.Admin.MaintenanceLog;
 
 namespace HotelIntegratedComputerSystems.Services.Admin
 {
     public class MaintenanceLogServices : BaseServices
     {
-        public PackageMaintenanceLogViewModel MaintenanceLogList()
+        public PackageMaintenanceLogViewModel GetMaintenanceLogList()
         {
             var maintLogList = from maintLog in Db.MaintenanceLogs
                            select new MaintenanceLogViewModel()
@@ -28,6 +30,22 @@ namespace HotelIntegratedComputerSystems.Services.Admin
             return new PackageMaintenanceLogViewModel() {MaintenanceLogsList = maintLogList.ToList()};
         }
 
+        public PackageMaintenanceLogViewModel RoomsForMaintenaneLogCreateEdit()
+        {
+            var roomsList = from rooms in Db.Rooms
+                select new RoomViewModel()
+                {
+                    Id = rooms.Id,
+                    BuildingId = rooms.BuildingId,
+                    BuildingName = rooms.Building.BuildingName,
+                    FloorNumber = rooms.FloorNumber,
+                    RoomNumber = rooms.RoomNumber,
+                    RoomStatusId = rooms.RoomStatusId,
+                    RoomStatus = rooms.RoomStatus.Description
+                };
+            return new PackageMaintenanceLogViewModel() {RoomsList = roomsList.ToList()};
+        }
+
         public void CreateNewMaintenanceLog(PackageMaintenanceLogViewModel model)
         {
             Db.MaintenanceLogs.Add(new MaintenanceLogs()
@@ -38,6 +56,48 @@ namespace HotelIntegratedComputerSystems.Services.Admin
                 MaintenanceTypesId = model.MaintenanceLog.MaintenanceTypeId        
             });
             Db.SaveChanges();
+        }
+
+        public void PostChangesForEdit(PackageMaintenanceLogViewModel model)
+        {
+            Db.Entry(new MaintenanceLogs()
+            {
+                Id = model.MaintenanceLog.Id,
+                Date = model.MaintenanceLog.Date,
+                MaintenanceTypesId = model.MaintenanceLog.MaintenanceTypeId,
+                RoomId = model.MaintenanceLog.RoomId,
+                Description = model.MaintenanceLog.Description
+            })
+            .State = EntityState.Modified;
+            Db.SaveChanges();
+        }
+
+        public void DeleteEntry(int id)
+        {
+            var foundMaintLog = Db.MaintenanceLogs.Find(id);
+            Db.MaintenanceLogs.Remove(foundMaintLog);
+            Db.SaveChanges();
+        }
+
+        public PackageMaintenanceLogViewModel FindEntryById(int id)
+        {
+            var maintLog = Db.MaintenanceLogs.Find(id);
+            return (new PackageMaintenanceLogViewModel
+            {
+                MaintenanceLog = new MaintenanceLogViewModel()
+                {
+                    Id = maintLog.Id,
+                    BuildingId = maintLog.Room.BuildingId,
+                    BuildingName = maintLog.Room.Building.BuildingName,
+                    Date = maintLog.Date,
+                    Description = maintLog.Description,
+                    Floor = maintLog.Room.FloorNumber,
+                    MaintenanceTypeId = maintLog.MaintenanceTypesId,
+                    MaintenanceType = maintLog.MaintenanceType.Type,
+                    RoomId = maintLog.RoomId,
+                    RoomNumber = maintLog.Room.RoomNumber
+                }
+            });
         }
     }
 }
