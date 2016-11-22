232f11c4ec54395cbf34203f3ff34bd59fb1921d
JoshuaWetzel <joshua.wetzel-2@selu.edu>
2016-11-21 21:59:58 -0600

    gridviewsetup

---
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Models/GridViewBuilding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HotelIntegratedComputerSystems.Models.Admin;
+
+namespace HotelIntegratedComputerSystems.Models
+{
+    public class GridViewBuilding
+    {
+        public List<GridViewFloors> FloorsList { get; set; }
+        public string BuildingName { get; set; }
+        public int BuildingId { get; set; }
+
+    }
+
+    public class GridViewFloors
+    {
+        public List<RoomViewModel> RoomsList { get; set; }
+        public int FloorNumber { get; set; }
+
+    }
+}
