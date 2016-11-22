70e35a476cafdd409665dcbd84f898850d3d4531
JoshuaWetzel <joshua.wetzel-2@selu.edu>
2016-11-22 16:11:38 -0600

    Revert "Merge branch 'CollinB' into JoshLaptop"
    This reverts commit a381b269c682667660356ce7f085a8cd7efb6d15, reversing
    changes made to ec6ce46fc4ad8c1fa08749704f921359f051f1e6.

--- cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Controllers/Login/GoogleAccount.cs
+++ cmps285HotelProject/HotelIntegratedComputerSystems/HotelIntegratedComputerSystems/Controllers/Login/GoogleAccount.cs
@@ -14,8 +14,8 @@ namespace HotelIntegratedComputerSystems.Controllers.Login
         private readonly EmployeeServies _services = new EmployeeServies();
 
         public static int Id;
-        public static int TypeId;
-        public static string TypeTitle;
+        public static int EmployeeTypeId;
+        public static string EmployeeTypeTitle;
         public static string Email;
         public static string Address;
         public static double Phone;
@@ -35,21 +35,19 @@ namespace HotelIntegratedComputerSystems.Controllers.Login
             //  error here. Fix..
             //var employee = Db.Employees.Find(emailAddress);
             //  emailAddress wont work in the find function because it is not a primary key? 
-            //  need a new table for our database
-            var employee = Db.Employees.FirstOrDefault(x => x.Email == Email);
+                //  need a new table for our database
+            var employee = Db.Employees.Find(emailAddress);
 
             //  now grab all of the employee information and set it in our public variables
-            TypeId = employee.EmployeeType.Id;
-            TypeTitle = employee.EmployeeType.Title;
+            EmployeeTypeId = employee.EmployeeTypeId;
+            EmployeeTypeTitle = employee.EmployeeType.Title;
+            Email = employee.Email;
             Address = employee.Address;
             Phone = employee.Phone;
             Name = employee.Name;
-            Authority = 0; //  NOTE: Change all these once database is up-to-date
-            System.Diagnostics.Debug.WriteLine("** The user Address = " + Address);
-            System.Diagnostics.Debug.WriteLine("** The user Phone = " + Phone);
-            System.Diagnostics.Debug.WriteLine("** The user Name = " + Name);
-            System.Diagnostics.Debug.WriteLine("** The user Authority = " + Authority);
-            System.Diagnostics.Debug.WriteLine("** The user ID = " + TypeTitle);
+            Authority = 0000000000000000000000; //  NOTE: Change all these once database is up-to-date
+            System.Diagnostics.Debug.WriteLine("** The user email = " + Email);
+
         }
 
 
@@ -57,8 +55,8 @@ namespace HotelIntegratedComputerSystems.Controllers.Login
         public void DisposeAccount()
         {
             Id = 0;
-            TypeId = 0;
-            TypeTitle = null;
+            EmployeeTypeId = 0;
+            EmployeeTypeTitle = null;
             Email = null;
             Address = null;
             Phone = 0;
